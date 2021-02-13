219e4821ae3b70d34bfafbe87fc34112110eae01
LeoIta <leonardolorusso83@gmail.com>
2021-02-13 13:11:34 +0100

    pom file

---
+++ src/main/java/com/sda/finalproject/BankApp.java
@@ -0,0 +1,11 @@
+package com.sda.finalproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankApp {
+    public static void main(String[] args) {
+        SpringApplication.run(BankApp.class,args);
+    }
+}
